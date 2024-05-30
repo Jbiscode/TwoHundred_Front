eecbbfb249146033475fe6d17a24cdde8813c16d
hyelinkim <lins26@naver.com>
2024-05-30 11:41:37 +0900

    Feat : 인덱스 페이지 생성(임시) - 배너 생성 : 버튼 클릭으로 넘어감 - 카테고리 : 아이콘 동일(카테고리 fix시 수정요망) - 추천 상품 : 임시본(사진/title/meta/price 임시 수정요망)

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,39 +1,35 @@
 // import { MainResponse } from "../api/mainApi.jsx";
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
+import { useState, useEffect } from 'react';
 
-import Sidebar from "@components/Sidebar";
-import Header from "@components/Header";
-import bannerImage1 from '../assets/images/banner/banner1_pc.png';
-import bannerImage2 from '../assets/images/banner/banner2_pc.png';
-import bannerImage3 from '../assets/images/banner/banner3_pc.png';
+import bannerImage1 from '@/assets/images/banner/banner1_mobile.png';
+import bannerImage2 from '@/assets/images/banner/banner2_mobile.png';
+import bannerImage3 from '@/assets/images/banner/banner3_mobile.png';
 
 import categoryImage_best from '../assets/images/icon/category_bestgoods.svg';
 import goodsItem from '../assets/images/goodsItem/goodsItem1.jpeg';
-
+import BasicLayout from '../layouts/BasicLayout';
 
 function IndexPage() {
-    const bannerImages = [
-        bannerImage1,
-        bannerImage2,
-        bannerImage3,
-      ];
     
-    const properties = {
-        duration: 5000,
-        transitionDuration: 500,
-        infinite: true,
-        indicators: true,
-        arrows: true,
-        pauseOnHover: true,
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handleSlideChange = (direction) => {
+        if (direction === 'prev') {
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? 2 : prevSlide - 1));
+        } else {
+        setCurrentSlide((prevSlide) => (prevSlide === 2 ? 0 : prevSlide + 1));
+        }
     };
+
+    useEffect(() => {
+        console.log('Current slide:', currentSlide);
+      }, [currentSlide]);
 
     const categoryList = [
         {
           value: 1,
           text: '인기상품',
           src: categoryImage_best
-        
         },
         {
           value: 2,
@@ -70,151 +66,158 @@ function IndexPage() {
           text: '스포츠/레저',
           src: categoryImage_best
         },
-        
-      ]
-
-    const goodsList =[
-        {
-            value:1,
-            title : '신상1',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:2,
-            title : '신상2',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:3,
-            title : '신상3',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:4,
-            title : '신상4',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:5,
-            title : '신상5',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:6,
-            title : '신상6',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:7,
-            title : '신상7',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:8,
-            title : '신상8',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:9,
-            title : '신상9',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-
-        },
-        {
-            value:10,
-            title : '신상10',
-            meta : '강남구 비트캠프',
-            src : goodsItem,
-            price: '10,000'
-
-        },
-
     ]
 
-  return (
-    <div className="main">
-      <div className="flex-1">
-        <Header />
-      </div>    
+    const goodsList = [
+        {
+            value: 1,
+            title: '신상1',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 2,
+            title: '신상2',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 3,
+            title: '신상3',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 4,
+            title: '신상4',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 5,
+            title: '신상5',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 6,
+            title: '신상6',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 7,
+            title: '신상7',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 8,
+            title: '신상8',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 9,
+            title: '신상9',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+        {
+            value: 10,
+            title: '신상10',
+            meta: '강남구 비트캠프',
+            src: goodsItem,
+            price: '10,000'
+        },
+    ]
 
-      <div className="banner-wrapper">
-      <Slide {...properties}>
-          {bannerImages.map((image, index) => (
-            <div key={index} className="each-slide">
-              <img src={image} alt={`Banner ${index + 1}`} />
-            </div>
-          ))}
-        </Slide>
-      </div>
+    return (
+        <BasicLayout>
+            <div>
+            <div className="flex flex-col">
 
-
-      <div className="category">
-        <div className="category-wrapper">
-            <div className="category-list">
-            {categoryList.map((category) => (
-                <div key={category.value} >
-                    <a href="#"><img src={category.src} alt={category.text} className="category-icn" /></a>
-                    <span className="category-text">{category.text}</span>
-                </div>
-            ))}
-            </div>
-        </div>
-      </div>
-
-    <div className="newgoods-title">신규상품</div>
-    
-    <div className="goods">
-        <div className="goods-wrapper">
-            <div className="goods-list">
-                {goodsList.map((item) => (
-                    <div key={item.value} className="goods-cont">
-                        <a href="#"><img src={item.src} alt={item.text} className="goods-icn" /></a>
-                        <span className="goods-cont_title">{item.title}</span>
-                        <span className="goods-cont_meta">{item.meta}</span>
-                        <span className="goods-cont_bottom"></span>
-                        <span className="goods-cont_price">{item.price}</span>
+            {/* 배너 */}
+            <div className="carousel w-full" data-interval="false">
+                <div className={`carousel-item relative w-full ${currentSlide === 0 ? 'block' : 'hidden'}`}>
+                    <img src={bannerImage1} className="w-full h-[180px]" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('prev')}>❮</button>
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('next')}>❯</button>
                     </div>
-                ))}
+                </div>
+                <div className={`carousel-item relative w-full ${currentSlide === 1 ? 'block' : 'hidden'}`}>
+                    <img src={bannerImage2} className="w-full h-[180px]" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('prev')}>❮</button>
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('next')}>❯</button>
+                    </div>
+                </div>
+                <div className={`carousel-item relative w-full ${currentSlide === 2 ? 'block' : 'hidden'}`}>
+                    <img src={bannerImage3} className="w-full h-[180px]" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('prev')}>❮</button>
+                        <button className="btn btn-circle" onClick={() => handleSlideChange('next')}>❯</button>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* 카테고리 */}
+            <div className="justify-center flex-col flex-grow ">
+                <div className="mt-7 mb-7  grid justify-center ">
+                    <div className="category-list p-2 w-full justify-center grid grid-cols-4">
+                    {categoryList.map((category) => (
+                        <div key={category.value} className="grid p-1">
+                            <a href="#" >
+                                <img src={category.src} alt={category.text} className="m-[20px] w-12 h-[35px] items-center" />
+                            </a>
+
+                            <div className="category-text font-family[Noto Sans] text-base h-full text-center grid items-center">
+                            {category.text}
+                            </div>
+                        </div>
+                    ))}
+                        </div>
+                    </div>
+                </div>
+                
+                {/* 추천상품 */}
+                <div className="newgoods-title mt-30 mx-6 text-lg font-bold">추천 상품</div>
+                <div className="goods">
+                    <div className="goods-wrapper w-full grid justify-center box-border">
+                        <div className="goods-list mr-7 ml-3 w-full grid box-border list-none  ">
+                            {goodsList.map((item) => (
+                                <div key={item.value} className="goods-cont ">
+                                    <a href="#"><img src={item.src} alt={item.title} className="goods-icn items-center max-w-[165px] h-[211px]" /></a>
+                                    <span className="w-full text-base font-medium  truncate h-5 break-words inline-block line-clamp-1">
+                                        {item.title}
+                                    </span>
+                                    <span className="text-sm  text-gray-500">
+                                        {item.meta}
+                                    </span>
+                                    <span className="flex justify-between goods-cont_bottom"></span>
+                                    <span className="text-lg font-extrabold goods-cont_price">
+                                        {item.price}
+                                    </span>                
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-  );
+        </BasicLayout>
+    );
 }
 
 export default IndexPage;
