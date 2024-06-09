9e20e7e59234a733e82314b64b4570f702c98e71
dongsu <tlsehd930@gmail.com>
2024-06-09 22:07:57 +0900

    FEAT : 마이페이지 페이징 - 페이지 블록사이즈 10 설정 / 이전 다음 버튼 생성 - 판매중 / 판매완료 검색 기능 - 최신순 / 낮은가격순 / 높은가격순 정렬 기능

--- src/components/profile/SaleComponent.jsx
+++ src/components/profile/SaleComponent.jsx
@@ -3,29 +3,41 @@ import { Link } from "react-router-dom";
 import CLOTHES from "@/assets/images/clothes.png"
 import useAuthStore from "@zustand/authStore"
 import {auth} from "@api/index"
+import useModalStore from "@zustand/modalStore"
 
 
 const SaleComponent = () => {
+    const {openLoginModal} = useModalStore(state => state)
+
+    const [sortBy, setSortBy] = useState('latest')
     const [tradeStatus, setTradeStatus] = useState('ON_SALE')
     const [page, setPage] = useState(1)
     const [pagingHTML, setPagingHTML] = useState([])
     const [mySalesDTO, setMySalesDTO] = useState([])
+    const [isExistPrev, setIsExistPrev] = useState(false)
+    const [isExistNext, setIsExistNext] = useState(false)
+    const [nextPage, setNextPage] = useState();
+    const [prevPage, setPrevPage] = useState();
+    
 
     useEffect(()=>{
         const fetchData = async () => {
             // 로그인이 필요한 정보받아오기
             try{
                 const response = await auth.get(
-                    `/api/v1/users/me/${tradeStatus}?page=${page}`,
+                    `/api/v1/users/me/${tradeStatus}/${sortBy}?page=${page}`,
                     {withCredentials: true}
                 )
             if(response.resultCode == '401'){
-                setIsModalOpen(true)
+                openLoginModal()
             }
             if(response.resultCode == '200'){
-                console.log(response.data.content)
-                setMySalesDTO(response.data.content)
-                setPagingHTML(Array.from({length : response.data.totalPages},(_, index) => index+1))
+                setMySalesDTO(response.data.mySalesResponses)
+                setPagingHTML(response.data.pageNumList)
+                setIsExistPrev(response.data.prev)
+                setIsExistNext(response.data.next)
+                setPrevPage(response.data.prevPage)
+                setNextPage(response.data.nextPage)
             }
             console.log(response.data)
             }catch(error){
@@ -34,29 +46,54 @@ const SaleComponent = () => {
         }
         
         fetchData();
-        console.log(pagingHTML)
-    },[page])
+    },[page,tradeStatus,sortBy])
+
+    const handleTradeStatusChangeSoldOut = () => {
+        setTradeStatus('SOLD_OUT')
+        setPage(1)
+    }
+
+    const handleTradeStatusChangeOnSale = () => {
+        setTradeStatus('ON_SALE')
+        setPage(1)
+    }
+
+    const handleSortChangeLatest = () => {
+        setSortBy('latest')
+        setPage(1)
+    }
+
+    const handleSortChangelowprice = () => {
+        setSortBy('low-price')
+        setPage(1)
+    }
+
+    const handleSortChangehighprice = () => {
+        setSortBy('high-price')
+        setPage(1)
+    }
+
 
     return (
         <div>
             <p className="font-bold pt-4 mb-3">판매 상품</p>
             <div>
                 <div className="w-full flex border-solid border-t-2 border-black text-lg">
-                    <div className="w-1/2 "><Link className="block w-full text-center py-3 text-base font-bold border-solid border-b-2 border-black">판매중</Link></div>
-                    <div className="w-1/2 "><Link className="block w-full text-center py-3 text-base font-bold border-solid border-b-2 border-gray-300">판매완료</Link></div>
+                    <div className="w-1/2 "><p className={`block w-full text-center py-3 text-base font-bold border-solid border-b-2 ${tradeStatus === 'ON_SALE' ? 'border-black' : 'border-gray-300'}`} onClick={handleTradeStatusChangeOnSale}>판매중</p></div>
+                    <div className="w-1/2 "><p className={`block w-full text-center py-3 text-base font-bold border-solid border-b-2 ${tradeStatus === 'SOLD_OUT' ? 'border-black' : 'border-gray-300'}`} onClick={handleTradeStatusChangeSoldOut}>판매완료</p></div>
                 </div>
             </div>
             <div className="flex text-base font-bold justify-end my-4 gap-2 items-center">
-                <p>최신순</p>
+                <p onClick={handleSortChangeLatest} className={sortBy === 'latest' ? 'text-orange-600' : ''}>최신순</p>
                 <p>|</p>
-                <p>낮은 가격순</p>
+                <p onClick={handleSortChangelowprice} className={sortBy === 'low-price' ? 'text-orange-600' : ''}>낮은 가격순</p>
                 <p>|</p>
-                <p>높은 가격순</p>
+                <p onClick={handleSortChangehighprice} className={sortBy === 'high-price' ? 'text-orange-600' : ''}>높은 가격순</p>
             </div>
             <div className="flex flex-wrap -mx-2">
                 {
                     mySalesDTO.map(item => (
-                        <div className="w-1/2 px-2 mb-4">
+                        <div className="w-1/2 px-2 mb-4" key={item.id}>
                             <div>
                             <img src={`https://kr.object.ncloudstorage.com/kjwtest/article/${item.thumbnailUrl}`} />
                             </div>
@@ -101,13 +138,16 @@ const SaleComponent = () => {
                     </div>
                 </div> */}
             </div>
-            <div>
-               
-                {
-                pagingHTML.map(item => (
-                    <span className={item == page ? "text-orange-600 font-bold" : ""} key={item} onClick={()=> setPage(item)}>{item}</span>
-                ))
-                }
+            <div className="flex justify-center">
+                <div className="flex gap-2">
+                    {isExistPrev && <span onClick={() => {setPage(prevPage)}}>이전</span>}
+                    {
+                    pagingHTML.map(item => (
+                        <span className={item == page ? "text-orange-600 font-bold" : ""} key={item} onClick={()=> setPage(item)}>{item}</span>
+                    ))
+                    }
+                    {isExistNext && <span onClick={() => {setPage(nextPage)}}>다음</span>}
+                </div>
             </div>
         </div>
     )
