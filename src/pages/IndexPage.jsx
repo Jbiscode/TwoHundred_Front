6d53fb1826dc27d5b8c90e2f36bb8d6574eac5d1
hyelinkim <lins26@naver.com>
2024-06-13 16:03:26 +0900

    ✨feat : 메인페이지 - 배너 이미지 변경 / 스타일 변경 - 카테고리 이미지 변경(헬스장비/회원권/스포츠웨어) - 카테고리 클릭 활성화 > search에서 데이터 추출 - 추천상품 페이징 20개 > 6개 추출 - 추천상품 스타일 변경(제품 상세 페이지 클릭 활성화 수정 요망)

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,17 +1,17 @@
 // import { MainResponse } from "../api/mainApi.jsx";
 import { useState, useEffect } from 'react';
 
-import bannerImage1 from '@assets/images/banner/banner1_mobile.png';
-import bannerImage2 from '@assets/images/banner/banner2_mobile.png';
-import bannerImage3 from '@assets/images/banner/banner3_mobile.png';
+import bannerImage1 from '@assets/images/banner/banner1.jpeg';
+import bannerImage2 from '@assets/images/banner/banner2.png';
+import bannerImage3 from '@assets/images/banner/banner3.jpeg';
 
 import equipment from '@assets/images/icon/equipment.png';
 import popularity from '@assets/images/icon/popularity.png';
 import food from '@assets/images/icon/food.png';
-import sportwear from '@assets/images/icon/sportwear.png';
+import sportwear from '@assets/images/icon/exercies.png';
 import supplies from '@assets/images/icon/supplies.png';
 import pt2 from '@assets/images/icon/pt2.png';
-import membership from '@assets/images/icon/membership.png';
+import membership from '@assets/images/icon/membership8.png';
 import newest from '@assets/images/icon/newest.png';
 
 
@@ -28,11 +28,13 @@ function IndexPage() {
     //배너
     const [currentSlide, setCurrentSlide] = useState(0);
     const { socket } = useSocketStore();
+
     //추천상품-페이징
     const [goodsList, setGoodsList] = useState([]);
     const [error, setError] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 20;
+    const itemsPerPage = 6;
+
 
     const handleSlideChange = (direction) => {
         if (direction === 'prev') {
@@ -56,51 +58,41 @@ function IndexPage() {
         return () => clearInterval(interval); 
     }, [socket]);
 
-    // 카테고리
+
+
+    // 카테고리 Enum 정의
+    const CategoryEnum = {
+        ORDER_OF_POPULARITY: 'ORDER_OF_POPULARITY',
+        NEWEST: 'NEWEST',
+        MEMBERSHIP: 'MEMBERSHIP',
+        PT: 'PT',
+        HEALTH_SUPPLIES: 'HEALTH_SUPPLIES',
+        HEALTH_EQUIPMENT: 'HEALTH_EQUIPMENT',
+        SPORT_WEAR: 'SPORT_WEAR',
+        FOOD: 'FOOD',
+    };
+
+    // 카테고리 목록 정의
     const categoryList = [
-        {
-          value: 1,
-          text: '인기순',
-          src: popularity
-        },
-        {
-          value: 2,
-          text: '최신순',
-          src: newest
-        },
-        {
-          value: 3,
-          text: '회원권',
-          src: membership
-        },
-        {
-          value: 4,
-          text: 'PT',
-          src: pt2
-        },
-        {
-          value: 5,
-          text: '헬스용품',
-          src: equipment
-        },
-        {
-          value: 6,
-          text: '헬스장비',
-          src: supplies
-        },
-        {
-          value: 7,
-          text: '스포츠웨어',
-          src: sportwear
-        },
-        {
-          value: 8,
-          text: '건강식품',
-          src: food
-        },
-    ]
+        { value: CategoryEnum.ORDER_OF_POPULARITY, text: '인기순', src: popularity },
+        { value: CategoryEnum.NEWEST, text: '최신순', src: newest },
+        { value: CategoryEnum.MEMBERSHIP, text: '회원권', src: membership },
+        { value: CategoryEnum.PT, text: 'PT', src: pt2 },
+        { value: CategoryEnum.HEALTH_SUPPLIES, text: '헬스용품', src: equipment },
+        { value: CategoryEnum.HEALTH_EQUIPMENT, text: '헬스장비', src: supplies },
+        { value: CategoryEnum.SPORT_WEAR, text: '스포츠웨어', src: sportwear },
+        { value: CategoryEnum.FOOD, text: '건강식품', src: food },
+    ];
 
-
+    const handleCategoryClick = async (category) => {
+        try {
+            const response = await instance(`/api/v1/search?category=${category.value}`);
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching category data:', error);
+        }
+    };
 
 
     useEffect(() => {
@@ -121,7 +113,6 @@ function IndexPage() {
                 setError(error);
             });
     }, []);
-
 
     if (error) {
         return <div>Error: {error.message}</div>;
@@ -153,22 +144,22 @@ function IndexPage() {
                 <div className={`carousel-item relative w-full ${currentSlide === 0 ? 'block' : 'hidden'}`}>
                     <img src={bannerImage1} className="w-full h-[180px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('prev')}>❮</button>
-                        <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('next')}>❯</button>
+                        <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('prev')}>❮</button>
+                        <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('next')}>❯</button>
                     </div>
                 </div>
                 <div className={`carousel-item relative w-full ${currentSlide === 1 ? 'block' : 'hidden'}`}>
                     <img src={bannerImage2} className="w-full h-[180px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('prev')}>❮</button>
-                    <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('next')}>❯</button>
+                    <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('prev')}>❮</button>
+                    <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('next')}>❯</button>
                     </div>
                 </div>
                 <div className={`carousel-item relative w-full ${currentSlide === 2 ? 'block' : 'hidden'}`}>
                     <img src={bannerImage3} className="w-full h-[180px]" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('prev')}>❮</button>
-                    <button className="btn btn-circle bg-transparent border-transparent" onClick={() => handleSlideChange('next')}>❯</button>
+                    <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('prev')}>❮</button>
+                    <button className="btn btn-circle bg-transparent border-transparent text-white" onClick={() => handleSlideChange('next')}>❯</button>
                     </div>
                 </div>
             </div>
@@ -178,10 +169,11 @@ function IndexPage() {
             <div className="justify-center flex-col flex-grow">
                 <div className="mt-7 mb-7 grid justify-center">
                     <div className="category-list p-3 w-full justify-center grid grid-cols-4">
-                        {categoryList.map((category, index) => (
-                            <div key={category.value || index} className="grid p-1">
-                                <a href="#">
-                                    <img src={category.src} alt={category.text} className="m-[20px] w-12 h-[35px] items-center" />
+                        {categoryList.map((category) => (
+                            <div key={category.value} className="grid p-1">
+                                <a href={`search?orderBy=latest&category=${category.value}`} 
+                                   onClick={() => handleCategoryClick(category)} >                          
+                                         <img src={category.src} alt={category.text} className="m-[20px] w-12 h-[35px] items-center" />
                                 </a>
                                 <div className="category-text text-m font-family[Noto Sans] text-base h-full text-center grid items-center">
                                     {category.text}
@@ -193,24 +185,31 @@ function IndexPage() {
             </div>
                 
             {/* 추천상품 */}
-            <div className="newgoods-title mt-30 mb-2 mx-7 text-lg font-bold">추천 상품</div>
+            <div className="newgoods-title mt-30 mb-4 mx-7 text-lg font-bold">추천 상품</div>
             <div className="goods">
-                <div className="goods-wrapper w-full grid justify-center box-border">
-                    <div className="goods-list mr-7 ml-3 w-full grid box-border list-none grid-cols-2 ">
+                <div className="goods-wrapper w-full  grid justify-center box-border">
+                    <div className="goods-list  w-full grid box-border list-none grid-cols-2 ">
                         {currentGoods.length > 0 ? (
                             currentGoods.map((item) => (
-                                <div key={item.id} className="goods-cont mb-4">
-                                    <a href="#"><img src={item.src} alt={item.title} className="goods-icn items-center max-w-[165px] h-[211px]" /></a>
-                                    <span className="w-full text-base font-medium truncate h-5 break-words inline-block line-clamp-1">
+                                <div key={item.id} className="goods-cont mb-7">
+                                    <a href="#">
+                                        <img 
+                                            src={`https://kr.object.ncloudstorage.com/kjwtest/article/${item.thumbnailUrl}`} 
+                                            alt={item.imageId} 
+                                            className="goods-icn mb-3 items-center max-w-[194px] h-[194px]" 
+                                            //onError={(e) => { e.target.onerror = null; e.target.src = 'default-image-url'; }} // 이미지 로드 실패 시 대체 이미지 설정
+                                        />
+                                    </a>
+                                    <span className="w-full ml-2  text-base font-medium truncate h-5 break-words inline-block line-clamp-1">
                                         {item.title}
                                     </span>
-                                    <span className="text-sm text-gray-500">
-                                        {item.meta}
+                                    <span className="text-sm font-extralight ml-2">
+                                        {item.content}
                                     </span>
-                                    <span className="flex justify-between goods-cont_bottom"></span>
-                                    <span className="text-lg font-extrabold goods-cont_price">
-                                        {item.price}
-                                    </span>                
+                                    <span className="flex justify-between goods-cont_bottom mb-3"></span>
+                                    <span className="text-lg font-extrabold goods-cont_price ml-2">
+                                        {Number(item.price).toLocaleString()}
+                                    </span>               
                                 </div>
                             ))
                         ) : (
