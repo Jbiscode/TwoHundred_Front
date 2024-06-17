dfe9a1a7459fde5da7fef39944260be0d8c0194a
maeng <soon5796735@naver.com>
2024-06-17 15:39:19 +0900

    ✨Feat: 데이터 중복 요청 문제 해결  + nav 사진 수정 & 글 제거

--- src/layouts/BasicFooter.jsx
+++ src/layouts/BasicFooter.jsx
@@ -4,6 +4,7 @@ import useFooterStore from '@zustand/footerStore';
 
 import home from '@assets/images/icon/home.png';
 import search from '@assets/images/icon/search.png';
+import location from '@assets/images/icon/location.png';
 import chat from '@assets/images/icon/chat.png';
 import mypage from '@assets/images/icon/my.png';
 
@@ -30,27 +31,27 @@ function BasicFooter() {
         // </div>
 
         <div className="sticky bottom-0 w-full z-10">
-        <footer className="footer footer-center p-2 px-7 bg-white text-base-content shadow-lg border-solid border-t-[1px] border-gray-300">
+        <footer className="footer footer-center p-3 px-7 bg-white text-base-content shadow-lg border-solid border-t-[1px] border-gray-300">
             <aside className="flex justify-between w-full">
                 <div onClick={openHomePage} className="flex flex-col items-center">
                     <img src={home} alt="home" className="w-6 h-6" />
-                    <span>홈</span>
+                    <span></span>
                 </div>
-                <div onClick={openSearchPage} className="flex flex-col items-center w-13">
+                <div onClick={openSearchPage} className="flex flex-col items-center">
                     <img src={search} alt="search" className="w-6 h-6" />
-                    <span>검색</span>
+                    <span></span>
                 </div>
-                <div onClick={openSearchPage} className="flex flex-col items-center w-13">
-                    <img src={search} alt="search" className="w-6 h-6" />
-                    <span>내 근처</span>
+                <div onClick={openSearchPage} className="flex flex-col items-center">
+                    <img src={location} alt="search" className="w-7 h-7" />
+                    <span></span>
                 </div>
-                <div onClick={openChatPage} className="flex flex-col items-center w-13">
+                <div onClick={openChatPage} className="flex flex-col items-center">
                     <img src={chat} alt="chat" className="w-7 h-7" />
-                    <span>채팅</span>
+                    <span></span>
                 </div>
-                <div onClick={openMyPage} className="flex flex-col items-center w-13">
-                    <img src={mypage} alt="mypage" className="w-8 h-8" />
-                    <span>마이페이지</span>
+                <div onClick={openMyPage} className="flex flex-col items-center">
+                    <img src={mypage} alt="mypage" className="w-9 h-9" />
+                    <span></span>
                 </div>
             </aside>
         </footer>
