6b6aad881900d8a8c480ae96f48a388b522335e3
hyelinkim <lins26@naver.com>
2024-06-20 14:58:58 +0900

    🔨FIX : 메인페이지 수정 - 추천상품 component(goods) 분리 - 좋아요 기능 추가 - 메인페이지 자잘한 오류 수정

---
+++ src/components/goods.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Goods = ({ data, isLoading, likeState, handleLikeChange, isArticleLikedByUser }) => {
+    return (
+        <div>
+            <div className="goods-wrapper -mx-2 w-full grid justify-center box-border">
+                <div className="newgoods-title mt-30 mb-4 mx-7 text-lg font-bold">추천 상품</div>
+                {isLoading ? (
+                    <div>로딩 중 ...</div>
+                ) : (
+                    <div className="goods-list w-full grid box-border list-none grid-cols-2">
+                        {data.length > 0 ? (
+                            data.map((item) => {
+                                const isLiked = likeState[item.id] || isArticleLikedByUser(item.id);
+                                const image = isLiked ? '/src/assets/images/icon/heart_fill.svg' : '/src/assets/images/icon/heart_blank.svg';
+                                
+                                return (
+                                    <Link key={item.id} className="goods-cont mb-7 px-2" to={`/post/${item.id}`}>
+                                        <div className='rounded-[10%] relative'>
+                                            <img 
+                                                src={`https://kr.object.ncloudstorage.com/kjwtest/article/${item.thumbnailUrl}`} 
+                                                alt={item.imageId} 
+                                                className="rounded-[10%] border-solid border-[1px] border-[#f1f1f1] goods-icn mb-3 items-center max-w-[194px] h-[194px] block w-full" 
+                                            />
+                                            <img 
+                                                className="absolute top-2 right-2" 
+                                                alt='like icon' 
+                                                src={image} 
+                                                onClick={(e) => handleLikeChange(e, item.id)} 
+                                            />
+                                        </div>
+                                        <div className='pl-1'>
+                                            <p className="text-[16px] whitespace-nowrap text-ellipsis overflow-hidden font-bold my-1">
+                                                {item.title}
+                                            </p>
+                                            <p className="my-1 flex text-sm gap-1 font-bold text-gray-400">
+                                                {item.content}
+                                            </p>
+                                            <div className="text-lx font-bold">
+                                                {Number(item.price).toLocaleString()} 원
+                                            </div>
+                                        </div>               
+                                    </Link>
+                                );
+                            })
+                        ) : (
+                            <div>데이터가 없습니다</div>
+                        )}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Goods;
