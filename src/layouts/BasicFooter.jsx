ec54eb8fe8f06b0a6b13f4e9b5d4b06046a4beb3
maeng <soon5796735@naver.com>
2024-06-24 01:18:37 +0900

    ✨Feat: footer 현재 페이지 색 표시

--- src/layouts/BasicFooter.jsx
+++ src/layouts/BasicFooter.jsx
@@ -7,19 +7,29 @@ import search from '@assets/images/icon/search.png';
 import location from '@assets/images/icon/location.png';
 import chat from '@assets/images/icon/chat.png';
 import mypage from '@assets/images/icon/my.png';
+import clickHome from '@assets/images/icon/clickHome.png';
+import clickSearch from '@assets/images/icon/clickSearch.png';
+import clickLocation from '@assets/images/icon/clickLocation.png';
+import clickChat from '@assets/images/icon/clickChats.png';
+import clickMypage from '@assets/images/icon/clickMy.png';
+
 
 function BasicFooter() {
     const navigate = useNavigate();
     const { currentPage, openHomePage, openSearchPage, openMyLocationPage, openChatPage, openMyPage } = useFooterStore();
 
     useEffect(() => {
-        if (currentPage!='') {
-            navigate(`/${currentPage}`);
+        if (currentPage) {
+            navigate(currentPage);
             window.scrollTo({ top: 0 });
-            useFooterStore.setState({ currentPage: '' });
+            console.log(currentPage);
         }
-    }, [currentPage]);
+    }, [currentPage, navigate]);
 
+    console.log(`Current Page: ${currentPage}`);
+
+
+    
 
     return (
         // <div className="sticky bottom-0 z-10">
@@ -34,23 +44,23 @@ function BasicFooter() {
         <footer className="footer footer-center p-3 px-7 bg-white text-base-content shadow-lg border-solid border-t-[1px] border-gray-300">
             <aside className="flex justify-between w-full">
                 <div onClick={openHomePage} className="flex flex-col items-center">
-                    <img src={home} alt="home" className="w-6 h-6" />
+                <img src={currentPage == '/' ? clickHome : home} alt="home" className="w-6 h-6" />
                     <span></span>
                 </div>
                 <div onClick={openSearchPage} className="flex flex-col items-center">
-                    <img src={search} alt="search" className="w-6 h-6" />
+                    <img src={currentPage == '/search' ? clickSearch : search} alt="search" className="w-6 h-6" />
                     <span></span>
                 </div>
                 <div onClick={openMyLocationPage} className="flex flex-col items-center">
-                    <img src={location} alt="search" className="w-7 h-7" />
+                <img src={currentPage == '/myLocation' ? clickLocation : location} alt="search" className="w-7 h-7" />
                     <span></span>
                 </div>
                 <div onClick={openChatPage} className="flex flex-col items-center">
-                    <img src={chat} alt="chat" className="w-7 h-7" />
+                    <img src={currentPage == '/chat/list' ? clickChat : chat} alt="search" className="w-7 h-7" />
                     <span></span>
                 </div>
                 <div onClick={openMyPage} className="flex flex-col items-center">
-                    <img src={mypage} alt="mypage" className="w-9 h-9" />
+                <img src={currentPage == '/users/me' ? clickMypage : mypage} alt="search" className="w-9 h-9" />
                     <span></span>
                 </div>
             </aside>
