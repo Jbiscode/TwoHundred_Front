7a229d805cfb343db28629ee775b2cca4681a2fd
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-06-24 00:46:36 +0900

    FEAT : 안읽은 메시지 상태관리 추가

--- src/zustand/conversationStore.js
+++ src/zustand/conversationStore.js
@@ -5,14 +5,24 @@ const conversationStore = create((set) => ({
   setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
   messages: [],
   setMessages: (messages) => set({ messages }),
+  postDetails:{},
+  setPostDetails: (postDetails) => set({ postDetails }),
   unreadCount: {},
-  setUnreadCount: (chatId, unreadCount) => set((state) => ({ unreadCount: { ...state.unreadCount, [chatId]: unreadCount } })),
+  setUnreadCount: (chatId, unreadCount) => set((state) => {
+    const newUnreadCount = { ...state.unreadCount, [chatId]: unreadCount };
+    const totalUnread = Object.values(newUnreadCount).reduce((sum, value) => sum + value, 0);
+    return { unreadCount: newUnreadCount, totalUnread };
+  }),
   lastMessage: {},
   setLastMessage: (chatId, lastMessage) => set((state) => ({ lastMessage: { ...state.lastMessage, [chatId]: lastMessage } })),
   newMessage: 0,
   setNewMessage: () => set((state) => ({ newMessage: state.newMessage + 1 })),
   modifiedDate: {},
   setModifiedDate: (chatId, modifiedDate) => set((state) => ({ modifiedDate: { ...state.modifiedDate, [chatId]: modifiedDate } })),
+  totalUnread:0,
+  updateTotalUnread: () => set((state) => ({
+    totalUnread: Object.values(state.unreadCount).reduce((sum, value) => sum + value, 0)
+  })),
 }));
 
 export default conversationStore;
