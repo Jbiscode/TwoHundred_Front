acfe1eb8ca7b637571585e73a59896cce5afa763
Jbiscode <99472056+Jbiscode@users.noreply.github.com>
2024-06-25 00:51:18 +0900

    FIX : 푸터 눌렀을때 색깔표시 수정

--- src/layouts/BasicFooter.jsx
+++ src/layouts/BasicFooter.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useFooterStore from '@zustand/footerStore';
 import useAuthStore from '@zustand/authStore';
@@ -20,6 +20,7 @@ import clickMypage from '@assets/images/icon/clickMy.png';
 function BasicFooter() {
     const navigate = useNavigate();
     const {conversations} = useGetConversations();
+    const [page, setPage]=useState("");
     useGetLastMessageAndUnread(conversations);
 
     const { currentPage, openHomePage, openSearchPage,openMyLocationPage, openChatPage, openMyPage } = useFooterStore();
@@ -34,6 +35,11 @@ function BasicFooter() {
         }
     }, [currentPage, alarmCount]);
 
+    useEffect(() => {
+        const path = window.location.pathname.split('/').slice(0, 2).join('/');
+        setPage(path);
+    }, []);
+
     
 
 
@@ -42,23 +48,23 @@ function BasicFooter() {
         <footer className="footer footer-center p-3 px-7 bg-white text-base-content shadow-lg border-solid border-t-[1px] border-gray-300">
             <aside className="flex justify-between w-full">
                 <div onClick={openHomePage} className="flex flex-col items-center">
-                <img src={currentPage == '/' ? clickHome : home} alt="home" className="w-6 h-6" />
+                <img src={page == '/' ? clickHome : home} alt="home" className="w-6 h-6" />
                     <span></span>
                 </div>
                 <div onClick={openSearchPage} className="flex flex-col items-center">
-                    <img src={currentPage == '/search' ? clickSearch : search} alt="search" className="w-6 h-6" />
+                    <img src={page == '/search' ? clickSearch : search} alt="search" className="w-6 h-6" />
                     <span></span>
                 </div>
                 <div onClick={openMyLocationPage} className="flex flex-col items-center">
-                <img src={currentPage == '/myLocation' ? clickLocation : location} alt="search" className="w-7 h-7" />
+                <img src={page == '/myLocation' ? clickLocation : location} alt="search" className="w-7 h-7" />
                     <span></span>
                 </div>
                 <div onClick={openChatPage} className="flex flex-col items-center relative">
-                    <img src={currentPage == '/chat/list' ? clickChat : chat} alt="search" className="w-7 h-7" />
+                    <img src={page == '/chat' ? clickChat : chat} alt="search" className="w-7 h-7" />
                     {alarmCount>0 ? <span className='absolute left-4 bottom-3 bg-red-500 text-white rounded-full px-2 py-1'>{alarmCount}</span>:null}
                 </div>
                 <div onClick={openMyPage} className="flex flex-col items-center">
-                <img src={currentPage == '/users/me' ? clickMypage : mypage} alt="search" className="w-9 h-9" />
+                <img src={page == '/users' ? clickMypage : mypage} alt="search" className="w-9 h-9" />
                     <span></span>
                 </div>
             </aside>
